9b1bf7bbbcfe5d8217ac98e4fcce84276a568579
Harini Janakiraman <harinijanakiraman@gmail.com>
2023-07-23 11:52:10 +1000

    update contact

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Buildship Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/rowyio/buildship-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://rowy.io/discord',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/rowyio/buildship-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
