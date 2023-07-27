1731de9ab0ad8c284a1c3c3c74ab6c3d887aee47
BeeBombshell <vbhavya269@gmail.com>
2023-07-27 13:54:44 +0530

    Added Buildship favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,6 +10,15 @@ const config: DocsThemeConfig = {
       </span>
     )
   },
+  head: () => (
+    <>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
+    </>
+  ),
+  useNextSeoProps: () => ({ titleTemplate: '%s – BuildShip' }),
   project: {
     link: 'https://github.com/rowyio/rowy', // Link to the project repository
   },
