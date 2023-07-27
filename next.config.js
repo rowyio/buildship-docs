cb8ef778f4bf1d823b6fc22ff0fd06da20ed5a1e
BeeBombshell <vbhavya269@gmail.com>
2023-07-27 17:43:37 +0530

    Setup Page structure and Home page redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const BASE_PATH = process.env.BASE_PATH;
+const PREFIX_PATH = "";
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+    basePath: BASE_PATH || "",
+    async redirects() {
+      return [
+        /*
+          Redirects to avoid inital page
+        */
+        {
+          source: "/",
+          destination: "/introduction/welcome",
+          permanent: true,
+        }
+      ];
+    },
+  })
