17c70e96a7e53921ead6ae84e30bf794c17afc13
BeeBombshell <vbhavya269@gmail.com>
2023-07-28 19:34:53 +0530

    Fixed SVG, meta desc, and sub-titles

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import Logo from './components/Logo'
 
 const config: DocsThemeConfig = {
@@ -10,14 +10,34 @@ const config: DocsThemeConfig = {
       </span>
     )
   },
-  head: () => (
-    <>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
-      <link rel="manifest" href="/site.webmanifest"></link>
-    </>
-  ),
+  gitTimestamp: null,
+  head() {
+    const { frontMatter } = useConfig();
+    return (
+        <>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+          <link rel="manifest" href="/site.webmanifest"></link>
+          
+          <meta
+            property="og:title" 
+            content={
+              frontMatter.title
+              ? frontMatter.title + " – BuildShip"
+              : "BuildShip Documentation"
+            } 
+          />
+          <meta
+          name="og:description"
+          content={
+            frontMatter.description ||
+            "AI-focused low-code backend workflow builder"
+          }
+        />
+        </>
+    );
+  },
   useNextSeoProps: () => ({ titleTemplate: '%s – BuildShip' }),
   project: {
     link: 'https://github.com/rowyio/rowy', // Link to the project repository
