0dd4314c812e676120210e430f5a10aa920bd072
Harini Janakiraman <harinijanakiraman@gmail.com>
2023-08-06 19:03:50 +1000

    Updated sidebar and design

--- pages/404.tsx
+++
@@ -1,9 +0,0 @@
-import PageNotFound from '../components/PageNotFound';
-
-export default function Page() {
-  return (
-    <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-      <PageNotFound />
-    </div>
-  );
-}
