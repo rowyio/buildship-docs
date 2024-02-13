9f2601bf1dfe73042872db5e5c7453ad0d89e1e4
BeeBombshell <vbhavya269@gmail.com>
2024-02-13 13:46:04 +0530

    Updated anchor target

--- components/features/help.tsx
+++ components/features/help.tsx
@@ -18,28 +18,9 @@ export function Help() {
               </svg>
               <div>
                 <strong>
-                  <a href='https://buildship.com/experts'>Hire a BuildShip Expert:</a>
-                </strong>
-                <p>
-                  Need to get your product ready fast? Browse and choose from a range of freelancers, agencies, and
-                  indie makers - well versed with BuildShip.
-                </p>
-              </div>
-            </li>
-            <li className='flex items-start pb-3'>
-              <svg
-                className='h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2 mr-3'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path
-                  d='m19 9l1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25zm-7.5.5L9 4L6.5 9.5L1 12l5.5 2.5L9 20l2.5-5.5L17 12zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25z'
-                  fill='none'
-                />
-              </svg>
-              <div>
-                <strong>
-                  <a href='https://buildship.com/discord'>The BuildShip Community:</a>
+                  <a href='https://buildship.com/discord' target='_blank'>
+                    The BuildShip Community:
+                  </a>
                 </strong>
                 <p>
                   An active community of low-code builders. Ask questions, seek guidance, share thoughts, showcase
@@ -60,7 +41,32 @@ export function Help() {
               </svg>
               <div>
                 <strong>
-                  <a href='https://docs.buildship.com/support-messages'>Send a Support Request:</a>
+                  <a href='https://buildship.com/experts' target='_blank'>
+                    Hire a BuildShip Expert:
+                  </a>
+                </strong>
+                <p>
+                  Need to get your product ready fast? Browse and choose from a range of freelancers, agencies, and
+                  indie makers - well versed with BuildShip.
+                </p>
+              </div>
+            </li>
+            <li className='flex items-start pb-3'>
+              <svg
+                className='h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2 mr-3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path
+                  d='m19 9l1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25zm-7.5.5L9 4L6.5 9.5L1 12l5.5 2.5L9 20l2.5-5.5L17 12zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25z'
+                  fill='none'
+                />
+              </svg>
+              <div>
+                <strong>
+                  <a href='https://docs.buildship.com/support-messages' target='_blank'>
+                    Send a Support Request:
+                  </a>
                 </strong>
                 <p>
                   Got a question, need to request for a feature, or see a bug? Send us a support request directly from
